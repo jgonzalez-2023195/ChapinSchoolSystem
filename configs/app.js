ec115ce6d1f2119eca84df347216aca548999d61
Jose Gonzalez <jgonzalez-2023195@kinal.edu.gt>
2025-02-09 22:38:44 -0600

    Se crean los archivos de la carpeta configs

---
+++ configs/app.js
@@ -0,0 +1,34 @@
+'use strict'
+
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors'
+import authRoutes from '../src/auth/auth.routes.js'
+import coursesRoutes from '../src/course/course.routes.js'
+
+const configs = (app)=> {
+    app.use(express.json())
+    app.use(express.urlencoded({extended: false}))
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan('dev'))
+    // app.use(limiter)
+}
+
+const routes = (app)=> {
+    app.use('/v1/schoolSystem', authRoutes)
+    app.use('/v1/courses', coursesRoutes)
+}
+
+export const initServer = ()=> {
+    const app = express()
+    try {
+        configs(app)
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running in port: ${process.env.PORT}`)
+    } catch (e) {
+        console.error('Server init failed: ', e);
+    }
+}
