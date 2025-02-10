f6dfc51f3807810a52bcbc11b11e1233e0698b52
Jose Gonzalez <jgonzalez-2023195@kinal.edu.gt>
2025-02-09 22:39:28 -0600

    Se crean los archivos de la carpeta utils

---
+++ utils/db.validators.js
@@ -0,0 +1,19 @@
+
+import User from '../src/user/user.model.js'
+import Course from '../src/course/course.model.js'
+
+export const existEmail = async(email, user)=>{
+    const alreadyEmail = await User.findOne({email})
+    if(alreadyEmail && alreadyEmail._id != user.uid){
+        console.error(`Email ${email} is already taken`)
+        throw new Error(`Email ${email} is already taken`)
+    }
+}
+
+export const existCourse = async(title, course)=> {
+    const alreadyCourse = await Course.findOne({title})
+    if(alreadyCourse && alreadyCourse._id != course._id){
+        console.error(`Course ${title} is already taken`)
+        throw new Error(`Course ${title} is already taken`)
+    }
+}
